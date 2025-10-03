472e058577f41363979d81f346a74f9c9f9fc6e3
Suryansh Ayush <22bds057@iiitdwd.ac.in>
2025-10-04 02:55:00 +0530

    Implemented OpenGL ES 2.0 texture renderer

---
+++ app/src/main/java/com/example/edgedetectionviewer/gl/CameraRenderer.kt
@@ -0,0 +1,165 @@
+package com.example.edgedetectionviewer.gl
+
+import android.graphics.Bitmap
+import android.graphics.SurfaceTexture
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import android.opengl.GLUtils
+import android.util.Log
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class CameraRenderer : GLSurfaceView.Renderer {
+
+    private val TAG = "CameraRenderer"
+
+    private var program = 0
+    private var textureId = 0
+    private var positionHandle = 0
+    private var texCoordHandle = 0
+    private var samplerHandle = 0
+
+    private lateinit var vertexBuffer: FloatBuffer
+    private lateinit var texCoordBuffer: FloatBuffer
+
+    var currentBitmap: Bitmap? = null
+        set(value) {
+            field = value
+            bitmapUpdated = true
+        }
+
+    private var bitmapUpdated = false
+
+    // Vertex coordinates (2 triangles forming a quad)
+    private val vertices = floatArrayOf(
+        -1.0f, -1.0f,  // Bottom-left
+        1.0f, -1.0f,   // Bottom-right
+        -1.0f, 1.0f,   // Top-left
+        1.0f, 1.0f     // Top-right
+    )
+
+    // Texture coordinates
+    private val texCoords = floatArrayOf(
+        0.0f, 1.0f,    // Bottom-left
+        1.0f, 1.0f,    // Bottom-right
+        0.0f, 0.0f,    // Top-left
+        1.0f, 0.0f     // Top-right
+    )
+
+    // Vertex shader
+    private val vertexShaderCode = """
+        attribute vec4 aPosition;
+        attribute vec2 aTexCoord;
+        varying vec2 vTexCoord;
+        
+        void main() {
+            gl_Position = aPosition;
+            vTexCoord = aTexCoord;
+        }
+    """.trimIndent()
+
+    // Fragment shader
+    private val fragmentShaderCode = """
+        precision mediump float;
+        varying vec2 vTexCoord;
+        uniform sampler2D uTexture;
+        
+        void main() {
+            gl_FragColor = texture2D(uTexture, vTexCoord);
+        }
+    """.trimIndent()
+
+    init {
+        // Initialize buffers
+        vertexBuffer = ByteBuffer.allocateDirect(vertices.size * 4)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
+            .put(vertices)
+        vertexBuffer.position(0)
+
+        texCoordBuffer = ByteBuffer.allocateDirect(texCoords.size * 4)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
+            .put(texCoords)
+        texCoordBuffer.position(0)
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+
+        // Create shader program
+        val vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+        program = GLES20.glCreateProgram()
+        GLES20.glAttachShader(program, vertexShader)
+        GLES20.glAttachShader(program, fragmentShader)
+        GLES20.glLinkProgram(program)
+
+        // Get handles
+        positionHandle = GLES20.glGetAttribLocation(program, "aPosition")
+        texCoordHandle = GLES20.glGetAttribLocation(program, "aTexCoord")
+        samplerHandle = GLES20.glGetUniformLocation(program, "uTexture")
+
+        // Generate texture
+        val textures = IntArray(1)
+        GLES20.glGenTextures(1, textures, 0)
+        textureId = textures[0]
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+
+        Log.i(TAG, "OpenGL ES surface created")
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+        Log.i(TAG, "Surface changed: ${width}x${height}")
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        // Update texture if bitmap changed
+        if (bitmapUpdated && currentBitmap != null) {
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId)
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, currentBitmap, 0)
+            bitmapUpdated = false
+        }
+
+        // Draw
+        GLES20.glUseProgram(program)
+
+        // Set vertex positions
+        GLES20.glEnableVertexAttribArray(positionHandle)
+        GLES20.glVertexAttribPointer(positionHandle, 2, GLES20.GL_FLOAT, false, 0, vertexBuffer)
+
+        // Set texture coordinates
+        GLES20.glEnableVertexAttribArray(texCoordHandle)
+        GLES20.glVertexAttribPointer(texCoordHandle, 2, GLES20.GL_FLOAT, false, 0, texCoordBuffer)
+
+        // Bind texture
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId)
+        GLES20.glUniform1i(samplerHandle, 0)
+
+        // Draw quad
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+
+        GLES20.glDisableVertexAttribArray(positionHandle)
+        GLES20.glDisableVertexAttribArray(texCoordHandle)
+    }
+
+    private fun loadShader(type: Int, shaderCode: String): Int {
+        val shader = GLES20.glCreateShader(type)
+        GLES20.glShaderSource(shader, shaderCode)
+        GLES20.glCompileShader(shader)
+        return shader
+    }
+}
